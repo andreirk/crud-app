a8e28a7e35d30a855e03e03b55089eb708945981
jackietana <213204740+jackietana@users.noreply.github.com>
2025-08-11 13:00:24 +0300

    update book_service.go

--- internal/service/book_service.go
+++ internal/service/book_service.go
@@ -6,7 +6,7 @@ import (
 	"github.com/jackietana/crud-app/internal/domain"
 )
 
-type BooksRepository interface {
+type BookRepository interface {
 	GetBookById(ctx context.Context, id int) (domain.Book, error)
 	GetBooks(ctx context.Context) ([]domain.Book, error)
 	CreateBook(ctx context.Context, book domain.BookCreate) error
@@ -14,30 +14,30 @@ type BooksRepository interface {
 	UpdateBook(ctx context.Context, id int, book domain.BookCreate) error
 }
 
-type BooksService struct {
-	repo BooksRepository
+type BookService struct {
+	repo BookRepository
 }
 
-func NewBooksService(repo BooksRepository) *BooksService {
-	return &BooksService{repo}
+func NewBookService(repo BookRepository) *BookService {
+	return &BookService{repo}
 }
 
-func (bs *BooksService) GetBooks(ctx context.Context) ([]domain.Book, error) {
+func (bs *BookService) GetBooks(ctx context.Context) ([]domain.Book, error) {
 	return bs.repo.GetBooks(ctx)
 }
 
-func (bs *BooksService) GetBookById(ctx context.Context, id int) (domain.Book, error) {
+func (bs *BookService) GetBookById(ctx context.Context, id int) (domain.Book, error) {
 	return bs.repo.GetBookById(ctx, id)
 }
 
-func (bs *BooksService) CreateBook(ctx context.Context, book domain.BookCreate) error {
+func (bs *BookService) CreateBook(ctx context.Context, book domain.BookCreate) error {
 	return bs.repo.CreateBook(ctx, book)
 }
 
-func (bs *BooksService) DeleteBook(ctx context.Context, id int) error {
+func (bs *BookService) DeleteBook(ctx context.Context, id int) error {
 	return bs.repo.DeleteBook(ctx, id)
 }
 
-func (bs *BooksService) UpdateBook(ctx context.Context, id int, book domain.BookCreate) error {
+func (bs *BookService) UpdateBook(ctx context.Context, id int, book domain.BookCreate) error {
 	return bs.repo.UpdateBook(ctx, id, book)
 }
