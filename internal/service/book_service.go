48cd3b5f5b4e7e9cfcd2670c7e3a010d80c5d3cc
jackietana <213204740+jackietana@users.noreply.github.com>
2025-08-15 10:57:23 +0300

    refactor code

--- internal/service/book_service.go
+++ internal/service/book_service.go
@@ -2,19 +2,9 @@ package service
 
 import (
 	"context"
-	"fmt"
-	"log"
-	"time"
 
-	"github.com/jackietana/cache-example"
 	"github.com/jackietana/crud-app/internal/domain"
-)
-
-const ttlDuration = time.Hour * 8
-
-var (
-	allBooksCached bool
-	cachedBookIDs  = make(map[string]string, 0)
+	"github.com/jackietana/crud-app/pkg"
 )
 
 type BookRepository interface {
@@ -26,91 +16,54 @@ type BookRepository interface {
 }
 
 type BookService struct {
-	repo  BookRepository
-	cache *cache.Cache
+	repo   BookRepository
+	cacher *pkg.CacheHandler
 }
 
 func NewBookService(repo BookRepository) *BookService {
-	return &BookService{repo, cache.New()}
+	return &BookService{repo, pkg.NewCacheHandler()}
 }
 
 func (bs *BookService) GetBooks(ctx context.Context) ([]domain.Book, error) {
-	var books = make([]domain.Book, 0)
-
-	if allBooksCached {
-		for _, id := range cachedBookIDs {
-			if id != "" {
-				item, _ := bs.cache.Get(id)
-				books = append(books, item.(domain.Book))
-			}
-		}
-
-		log.Println("SERVICE: all cached books retrieved")
-		return books, nil
+	books, err := bs.cacher.GetCachedBooks()
+	if err == nil {
+		return books, err
 	}
 
-	books, err := bs.repo.GetBooks(ctx)
-
-	for _, book := range books {
-		bookID := fmt.Sprintf("book_%d", book.ID)
-
-		if item, _ := bs.cache.Get(bookID); item == nil {
-			bs.cache.Set(bookID, book, ttlDuration)
-			cachedBookIDs[bookID] = bookID
-			log.Printf("SERVICE: %s added to cache", bookID)
-		}
-	}
-
-	allBooksCached = true
+	books, err = bs.repo.GetBooks(ctx)
+	bs.cacher.AddBooks(books)
 
 	return books, err
 }
 
 func (bs *BookService) GetBookById(ctx context.Context, id int) (domain.Book, error) {
-	bookID := fmt.Sprintf("book_%d", id)
-
-	if val, err := bs.cache.Get(bookID); err == nil {
-		if book, ok := val.(domain.Book); ok {
-			log.Printf("SERVICE: %s retrieved from cache", bookID)
-			return book, nil
-		}
+	book, err := bs.cacher.GetCachedBook(id)
+	if err == nil {
+		return book, err
 	}
 
-	book, err := bs.repo.GetBookById(ctx, id)
+	book, err = bs.repo.GetBookById(ctx, id)
 	if err == nil {
-		bs.cache.Set(bookID, book, ttlDuration)
-		cachedBookIDs[bookID] = bookID
-		log.Printf("SERVICE: book_%d added to cache", id)
+		bs.cacher.AddBook(book)
 	}
 
 	return book, err
 }
 
 func (bs *BookService) CreateBook(ctx context.Context, book domain.Book) error {
-	allBooksCached = false
+	bs.cacher.UpdateCacher()
+
 	return bs.repo.CreateBook(ctx, book)
 }
 
 func (bs *BookService) DeleteBook(ctx context.Context, id int) error {
-	bookId := fmt.Sprintf("book_%d", id)
-
-	if _, err := bs.cache.Get(bookId); err == nil {
-		bs.cache.Delete(bookId)
-		delete(cachedBookIDs, bookId)
-		log.Printf("SERVICE: %s removed from cache", bookId)
-	}
+	bs.cacher.DeleteCachedBook(id)
 
 	return bs.repo.DeleteBook(ctx, id)
 }
 
 func (bs *BookService) UpdateBook(ctx context.Context, id int, book domain.Book) error {
-	bookId := fmt.Sprintf("book_%d", id)
-
-	if _, err := bs.cache.Get(bookId); err == nil {
-		bs.cache.Delete(bookId)
-		allBooksCached = false
-		log.Printf("SERVICE: %s removed from cache", bookId)
-	}
+	bs.cacher.UpdateCachedBook(id)
 
 	return bs.repo.UpdateBook(ctx, id, book)
 }
---
+++ pkg/cacher.go
@@ -0,0 +1,100 @@
+package pkg
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/jackietana/cache-example"
+	"github.com/jackietana/crud-app/internal/domain"
+)
+
+const ttlDuration = time.Hour * 8
+
+var (
+	allBooksCached bool
+	cachedBookIDs  = make(map[string]string, 0)
+)
+
+type CacheHandler struct {
+	cache *cache.Cache
+}
+
+func NewCacheHandler() *CacheHandler {
+	return &CacheHandler{cache.New()}
+}
+
+func (ch *CacheHandler) GetCachedBooks() ([]domain.Book, error) {
+	var books = make([]domain.Book, 0)
+
+	if allBooksCached {
+		for _, id := range cachedBookIDs {
+			if id != "" {
+				item, err := ch.cache.Get(id)
+				if err != nil {
+					return nil, err
+				}
+
+				if book, ok := item.(domain.Book); ok {
+					books = append(books, book)
+				}
+			}
+		}
+
+		log.Println("CACHER: all cached books retrieved")
+		return books, nil
+	}
+
+	return nil, errors.New("not all books are in cache")
+}
+
+func (ch *CacheHandler) AddBook(book domain.Book) {
+	bookID := fmt.Sprintf("book_%d", book.ID)
+
+	if item, _ := ch.cache.Get(bookID); item == nil {
+		ch.cache.Set(bookID, book, ttlDuration)
+		cachedBookIDs[bookID] = bookID
+		log.Printf("CACHER: %s added to cache", bookID)
+	}
+}
+
+func (ch *CacheHandler) AddBooks(books []domain.Book) {
+	for _, book := range books {
+		ch.AddBook(book)
+	}
+
+	allBooksCached = true
+}
+
+func (ch CacheHandler) GetCachedBook(id int) (domain.Book, error) {
+	bookID := fmt.Sprintf("book_%d", id)
+
+	if val, err := ch.cache.Get(bookID); err == nil {
+		if book, ok := val.(domain.Book); ok {
+			log.Printf("CACHER: %s retrieved from cache", bookID)
+			return book, nil
+		}
+	}
+
+	return domain.Book{}, errors.New(bookID + "not found")
+}
+
+func (ch *CacheHandler) DeleteCachedBook(id int) {
+	bookId := fmt.Sprintf("book_%d", id)
+
+	if _, err := ch.cache.Get(bookId); err == nil {
+		ch.cache.Delete(bookId)
+		delete(cachedBookIDs, bookId)
+		log.Printf("CACHER: %s removed from cache", bookId)
+	}
+}
+
+func (ch *CacheHandler) UpdateCachedBook(id int) {
+	ch.DeleteCachedBook(id)
+	allBooksCached = false
+}
+
+func (ch *CacheHandler) UpdateCacher() {
+	allBooksCached = false
+}
