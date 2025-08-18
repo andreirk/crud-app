5aa1d6f32f02979226a4c56594abbbbd8820194a
jackietana <213204740+jackietana@users.noreply.github.com>
2025-08-18 09:35:56 +0300

    add middleware

--- internal/transport/rest/middleware.go
+++ internal/transport/rest/middleware.go
@@ -1,3 +1,22 @@
 package rest
 
-// TODO
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+)
+
+func loggerMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		startTime := time.Now()
+		ctx.Next()
+
+		endTime := time.Since(startTime).String()
+		log.WithFields(log.Fields{
+			"method":   ctx.Request.Method,
+			"URI":      ctx.Request.RequestURI,
+			"duration": endTime,
+		}).Info("Middleware: loggerMiddleware")
+	}
+}
