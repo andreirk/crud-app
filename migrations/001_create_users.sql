880eef1c5de67b20d52b2901c21e1e36baec055e
jackietana <213204740+jackietana@users.noreply.github.com>
2025-08-31 17:03:10 +0300

    add users.sql

---
+++ migrations/001_create_users.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id SERIAL NOT NULL UNIQUE,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    registered_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
