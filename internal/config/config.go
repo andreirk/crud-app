582246dbf758e23f2b59fee1b2606a7de9511dfb
jackietana <213204740+jackietana@users.noreply.github.com>
2025-08-31 17:02:27 +0300

    update config & handler

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,28 +2,43 @@ package config
 
 import (
 	"errors"
+	"time"
 
 	"github.com/spf13/viper"
 )
 
 type Config struct {
-	DB Postgres
+	DB     Postgres
+	Salt   string
+	Secret []byte
 
 	Server struct {
 		Port int `mapstructure:"port"`
 	} `mapstructure:"server"`
+
+	Auth struct {
+		TokenTTL time.Duration `mapstructure:"token_ttl"`
+	} `mapstructure:"auth"`
+}
+
+type Postgres struct {
+	Host string
+	Port string
+	User string
+	Pass string
+	Name string
 }
 
 func (c *Config) mapPostgres() error {
-	c.DB.Host = c.getField("host")
-	c.DB.Port = c.getField("port")
-	c.DB.User = c.getField("user")
-	c.DB.Pass = c.getField("pass")
-	c.DB.Name = c.getField("name")
+	c.DB.Host = c.getField("db_host")
+	c.DB.Port = c.getField("db_port")
+	c.DB.User = c.getField("db_user")
+	c.DB.Pass = c.getField("db_pass")
+	c.DB.Name = c.getField("db_name")
 
 	if c.DB.Host == "" || c.DB.Port == "" ||
 		c.DB.User == "" || c.DB.Pass == "" || c.DB.Name == "" {
-		return errors.New("unable to retrieve all fields")
+		return errors.New("unable to map all fields")
 	}
 
 	return nil
@@ -37,12 +52,24 @@ func (c *Config) getField(field string) string {
 	return ""
 }
 
-type Postgres struct {
-	Host string
-	Port string
-	User string
-	Pass string
-	Name string
+func (c *Config) getSalt() error {
+	val, ok := viper.Get("hash_salt").(string)
+	if ok {
+		c.Salt = val
+		return nil
+	}
+
+	return errors.New("unable to retrieve salt")
+}
+
+func (c *Config) getSecret() error {
+	val, ok := viper.Get("jwt_secret").(string)
+	if ok {
+		c.Secret = []byte(val)
+		return nil
+	}
+
+	return errors.New("unable to retrieve secret")
 }
 
 func New(dir, file string) (*Config, error) {
@@ -50,7 +77,6 @@ func New(dir, file string) (*Config, error) {
 
 	viper.AddConfigPath(dir)
 	viper.SetConfigName(file)
-	viper.SetEnvPrefix("db")
 	viper.AutomaticEnv()
 
 	if err := viper.ReadInConfig(); err != nil {
@@ -62,6 +88,14 @@ func New(dir, file string) (*Config, error) {
 	}
 
 	if err := cfg.mapPostgres(); err != nil {
+		return nil, err
+	}
+
+	if err := cfg.getSalt(); err != nil {
+		return nil, err
+	}
+
+	if err := cfg.getSecret(); err != nil {
 		return nil, err
 	}
 
